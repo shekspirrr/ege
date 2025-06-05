554602f41dfce0b04d04017ab125be1f017181d1
shekspirrr <danay.salfetnik@mail.ru>
2025-06-05 11:42:44 +0300

    Add files via upload

---
+++ №11.py
@@ -0,0 +1,47 @@
+'''from math import*
+for d in range(1,1000):
+    n = 10+52+458
+    c = ceil(log2(n))
+    num = ceil(d*c/8)
+    if 862*num<=276*1024:
+        print(d)'''
+
+'''from math import*
+for d in range(1,1000):
+    c = ceil(log2(d))
+    num = (261*c/8)
+    if 252500*num>31*1024*1024:
+        print(d)
+        break'''
+
+'''from math import*
+n = 10
+c = ceil(log2(n))
+id = ceil(217*c/8)
+print(81*id)'''
+
+'''from math import*
+for d in range(1,10000):
+    n = 10+26+450
+    c = ceil(log2(n))
+    num = ceil(d*c/8)
+    if 708*num>213*1024:
+        print(d)
+        break'''
+
+'''from math import*
+for n in range(1,1000):
+    c = ceil(log2(n))
+    num = (1231*c/8)
+    if 523872*num>432*1024*1024:
+        print(n)
+        break'''
+#17935
+'''from math import*
+for d in range(1,1000):
+    n = 10+999
+    c = ceil(log2(n))
+    num = ceil(745*c/8)
+    if 312*(num+d)<=311*1024:
+        print(d*312)
+'''
